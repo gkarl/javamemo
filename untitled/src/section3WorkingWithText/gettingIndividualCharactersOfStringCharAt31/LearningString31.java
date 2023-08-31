6bb7e52db26f4f61f5bea4555eb631001f5a06be
Karl Gavillot <ksgavillot@hotmail.com>
2023-08-31 10:09:34 +0200

    Section 3 -31 Getting Individual Characters of a String - charAt()

---
+++ untitled/src/section3WorkingWithText/gettingIndividualCharactersOfStringCharAt31/LearningString31.java
@@ -0,0 +1,23 @@
+package section3WorkingWithText.gettingIndividualCharactersOfStringCharAt31;
+
+/**
+ * Section 3 Working Whith Text - Getting Individual Characters of a String - charAt()
+ *
+ * recupérer 1 lettre d'1 string
+ *
+ * EX souvent on imprime que les initiale d'un prénom
+ */
+class LearningString31 {
+
+    public static void main(String[] args) {
+
+        String middleName = "Christopher";
+
+        int length = middleName.length();
+        
+        if (length > 5) {  // charAt()  => crée une erreur si on dépasse l'index max du string pour l'éviter on met cette condition
+            System.out.println(middleName.charAt(5)); // charAt() => nous renvoie simplement le caractère vers lequel nous pointons avec un index
+        }
+
+    }
+}
