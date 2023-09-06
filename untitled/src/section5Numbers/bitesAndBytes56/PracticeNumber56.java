e80730bea7d338a5c563eac8d97caca1a6f43160
Karl Gavillot <ksgavillot@hotmail.com>
2023-09-06 16:02:13 +0200

    Section 5 Number - 56 Bits and Bytes

---
+++ untitled/src/section5Numbers/bitesAndBytes56/PracticeNumber56.java
@@ -0,0 +1,19 @@
+package section5Numbers.bitesAndBytes56;
+
+/**
+ * Section 5 -56 Bits and Bytes
+ *
+ * Un ordinateur stock info sous forme de Byte (octet) composé de 8 bits (0 ou 1)
+ *
+ * 8    7    6    5    4    3    2    1   (Numero de la case)
+ * 128  64   32   16   8    4    2    1
+ *
+ *      1    0    0    0    0    0    1  (= 65 Binary encoding (codage binaire) = A en ASCII)
+ *
+ *                          1    1    1  (= 7 Binary encoding (codage binaire))
+ *
+ *                     1    1    0    1   (= 13 Binary encoding (codage binaire))
+ *
+ */
+class PracticeNumber56 {
+}
