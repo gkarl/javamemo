cebbcd31221c1a4d7d2d40cfd5c2db94149fc20b
Karl Gavillot <ksgavillot@hotmail.com>
2023-09-07 16:19:59 +0200

    Section 5 - 59 Bigger Numbers

---
+++ untitled/src/section5Numbers/biggerNumbers59/PracticeNumbers59.java
@@ -0,0 +1,12 @@
+package section5Numbers.biggerNumbers59;
+
+/**
+ * Section 5 -59 Bigger Numbers
+ *
+ * - int => 32 bites = 4 Bytes (octets) => [-2 147 483 648 à 2 147 483 648] milliards
+ *
+ * - Long => 64 bites = 8 Bytes (octets) = [-9 223 372 036 854 775 808 à  9 223 372 036 854 775 808]
+ * Ex stocker ID
+ */
+class PracticeNumbers59 {
+}
--- untitled/src/section5Numbers/numericDataType58/PracticeNumbers58.java
+++ untitled/src/section5Numbers/numericDataType58/PracticeNumbers58.java
@@ -9,7 +9,7 @@ package section5Numbers.numericDataType58;
  *
  * - Bollean  => true ou false il requiére que 1 bite (0 ou 1)
  *
- * - Byte => nbre compris entre [-128 ... 127] ou 255 (si on n'utilise pas de valeur négative)
+ * - Byte => nbre compris entre [-128 ... 127] ou 255 (si on n'utilise pas de valeur négative), utilise 1 seul Byte (octet)
  *
  * 8    7    6    5    4    3    2    1   (Numero de la case)
  * 128  64   32   16   8    4    2    1   (Valeur de la case => le dernier bite, le max (ex128) est souvent utilisé pour donner valeur négative)
@@ -24,6 +24,6 @@ class PracticeNumbers58 {
     public static void main(String[] args) {
 
         byte myByte = 13;
-        boolean myFlag = true; //
+        boolean myFlag = true;
     }
 }
