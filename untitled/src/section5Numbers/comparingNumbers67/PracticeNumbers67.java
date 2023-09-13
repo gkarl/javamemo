181a3cb7c6260133535b98e7cc260711125e0894
Karl Gavillot <ksgavillot@hotmail.com>
2023-09-13 18:53:07 +0200

    Section 5 - 67 Comparing Numbers

---
+++ untitled/src/section5Numbers/comparingNumbers67/PracticeNumbers67.java
@@ -0,0 +1,21 @@
+package section5Numbers.comparingNumbers67;
+
+/**
+ * Section 5 - 67 Comparing Numbers
+ *
+ * Comparer des nbre
+ * RQ souvent utilisé dans la condition d'1 if()
+ */
+class PracticeNumbers67 {
+
+    public static void main(String[] args) {
+
+        int numA = 5;
+        int numB = 7;
+
+        System.out.println(numA == numB); // == => déterminer si deux valeurs, deux nombres dans ce cas sont égaux ou non | return true ou false
+        System.out.println(numA >= numB); // >= => supérieur ou égale | return true ou false
+        System.out.println(numA != numB); // != => différent | return true ou false
+
+    }
+}
