02accc7d6c69bf00f20e9ee7941125a04a5863f0
Karl Gavillot <ksgavillot@hotmail.com>
2023-09-28 18:32:43 +0200

    Section 6 - 88 A Simpler For Loop

---
+++ untitled/src/section6ControlFlow/simpleForLoop88/PracticeControlFlow88.java
@@ -0,0 +1,16 @@
+package section6ControlFlow.simpleForLoop88;
+
+/**
+ * Section 6 Control Flow - 88 A Simpler For Loop
+ *
+ * un exemple simple de la boucle for pour etre sur qu'on a capté
+ */
+class PracticeControlFlow88 {
+
+    public static void main(String[] args) {
+
+        for (int counter = 1; counter <= 10; counter++) {
+            System.out.printf("décompte %d %n", counter);
+        }
+    }
+}
