f46309af30e7c064d8be538c3859aae834f6bf54
Karl Gavillot <ksgavillot@hotmail.com>
2023-10-22 19:34:20 +0200

    Section 8 - 130 Lambdas Versus Anonymous Classes

---
+++ untitled/src/section8OOP/lambdaVersusAnonymousClasses130/IEmployee.java
@@ -0,0 +1,6 @@
+package section8OOP.lambdaVersusAnonymousClasses130;
+
+//___________3____________ Créer cette interface avec une seule methode (important) et qui a la meme signature que la Lambda
+interface IEmployee {
+    int getSalary();
+}
